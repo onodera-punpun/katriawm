4611f1bb242c9e6505f6bbede8ffcca57949d24f
Camille Scholtz <camille@airmail.cc>
2017-06-22 18:14:08 +0200

    Add own theme

---
+++ themes/punpun/colorsfonts.h
@@ -0,0 +1,9 @@
+static char *dec_fonts[] = {
+    "Sans:pixelsize=10:style=bold:antialias=false",  /* FontTitle */
+};
+
+static char *dec_font_colors[] = {
+    "#000000",  /* DecStateNormal */
+    "#000000",  /* DecStateSelect */
+    "#000000",  /* DecStateUrgent */
+};
